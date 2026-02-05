f407c87c154462ec6b2b76b96c81d3abd7422d5a
Luís Otávio <143001279+LuisOtavioPessoa@users.noreply.github.com>
2026-02-05 16:23:26 -0300

    Adicionar novo currículo

--- src/app/curriculo/page.tsx
+++ src/app/curriculo/page.tsx
@@ -2,7 +2,7 @@ export default function CurriculoPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-black">
       <iframe
-        src="/curriculo.pdf"
+        src="/CV_Luis_Otavio.pdf"
         className="w-full h-screen border-none"
       />
     </div>
